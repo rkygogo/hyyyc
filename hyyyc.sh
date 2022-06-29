2a6220d8e466d255cf4e386e1d40698f5ea43600
rkygogo <107276912+rkygogo@users.noreply.github.com>
2022-06-29 16:12:21 +0800

    Update hyyyc.sh

--- hyyyc.sh
+++ hyyyc.sh
@@ -1,22 +1,23 @@
 #!/bin/sh
-
+mkdir -p /etc/hysteria
 hysteria_port="14000"
 hysteria_obfs="ycycxz"
 hysteria_passwd="ycycxz"
 
 killall hysteria
-wget -qO /usr/local/bin/hysteria https://cdn.jsdelivr.net/gh/none-blue/hysteria_amd64@main/hysteria
-wget -qO /usr/local/bin/xray https://cdn.jsdelivr.net/gh/none-blue/xray_amd64@main/xray
-chmod +x /usr/local/bin/{hysteria,xray}
+version=`wget -qO- -t1 -T2 --no-check-certificate "https://api.github.com/repos/HyNetwork/hysteria/releases/latest" | grep "tag_name" | head -n 1 | awk -F ":" '{print $2}' | sed 's/\"//g;s/,//g;s/ //g'`
+	echo -e "The Latest hysteria version:"`echo "${version}"`"\nDownload..."
+    get_arch=`arch`
+    if [ $get_arch = "x86_64" ];then
+        wget -N https://github.com/rkygogo/yy/blob/main/hysteria-tun-linux-amd64 -O /usr/bin/hysteria
+	
+    else
+        wget -N --no-check-certificate https://github.com/HyNetwork/hysteria/releases/download/${version}/hysteria-linux-arm64 -O /usr/bin/hysteria
+fi
+chmod +x /usr/bin/hysteria
 
-mkdir -p /etc/hysteria
-xray tls cert --ca \
---domain="cdn" \
---name="cdn" \
---org="cdn" \
---expire=8760000h \
---file=/etc/hysteria/ \
---json=false
+openssl ecparam -genkey -name prime256v1 -out /etc/hysteria/ca.key
+openssl req -new -x509 -days 36500 -key /etc/hysteria/ca.key -out /etc/hysteria/ca.crt -subj "/CN=www.baidu.com"
 
 cat << EOF > /etc/hysteria/serve.json
 {
